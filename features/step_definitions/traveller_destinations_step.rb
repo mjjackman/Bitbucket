965fc95cbc3b8ae0ad48ecd18ae8f776e4bb94e7
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-10 17:41:13 +0100

    added test for travellers to add new destinations to their bucket list, including stubbing out a registered traveller, and a session. Tests passing.

--- features/step_definitions/traveller_destinations_step.rb
+++ features/step_definitions/traveller_destinations_step.rb
@@ -7,14 +7,20 @@ Given(/^they are registered$/) do
   @traveller = Traveller.create!(:name => "Jon McDonald")
 end
 
-When(/^they are on the add destination page$/) do
-  visit new_destination_path
+Given(/^they are signed in$/) do
+  page.set_rack_session({ "warden.user.traveller.key" => [[@traveller.id], "some stuff"] })
+end
+
+When(/^they are on the add traveller destination page$/) do
+  visit new_traveller_destination_path(@traveller)
 end
 
 When(/^they choose a destination$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  save_and_open_page
+  select @destination.name, :from => 'traveller_destination[id]'
+  click_on 'Add'
 end
 
 Then(/^there is a destination in their bucket list$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(@traveller.destinations.count).to eq 1
 end
--- features/traveller_destinations.feature
+++ features/traveller_destinations.feature
@@ -3,6 +3,7 @@ Feature: Traveller Destinations
   Scenario: A traveller adds a destination
     Given there are some destinations
       And they are registered
-    When they are on the add destination page
+      And they are signed in
+    When they are on the add traveller destination page
       And they choose a destination
     Then there is a destination in their bucket list
