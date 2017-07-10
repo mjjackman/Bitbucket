029886125092b54d988a494ba3386d5d46db5be1
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-10 17:46:32 +0100

    created a form allowing users to select destinations by id in collection_select, added strong params permissions, and working new and create methods for traveller_destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,5 +1,34 @@
 class DestinationsController < ApplicationController
   
   def new
+    @destination = Destination.new
   end
+
+  def create
+   # binding.pry
+    @traveller = Traveller.find(params[:traveller_id])
+    @destination = @traveller.destinations.new(traveller_destination_params)
+    if @destination.save
+      redirect_to url_for(traveller_path(params[:traveller_id]))
+    else
+      flash[:message] = "Didn't work..."
+      render(:new)
+    end
+  end
+
+  private
+
+  def traveller_destination_params
+      params.require(:traveller_destination).permit(:destination_id)
+   end
+
+  # def find_provider_id
+  #   @provider = Provider.find(params[:provider_id])
+  # end
+
+  # def find_id
+  #  @offering = Offering.find(params[:id])
+  # end
+
+
 end
