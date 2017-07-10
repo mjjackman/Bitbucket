4ff145df435dc4c12c947658f0321b945fb1b8e1
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-10 22:09:45 +0100

    added a show method for travellers that brought in the destinations belonging to a specific traveller(by id), and any to_dos related to that destination.

--- app/controllers/travellers_controller.rb
+++ app/controllers/travellers_controller.rb
@@ -1,7 +1,9 @@
 class TravellersController < ApplicationController
-
+before_action :authenticate_traveller!
   def show
+   # binding.pry
     @traveller = Traveller.find(params[:id])
+    @destinations = @traveller.destinations.all
     render 'show'
   end
 
