2fb23c6312b18546c12dc07000839137a5caf77a
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-10 17:42:50 +0100

    added rack session to test environment for use in stubbing out login in tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :destinations, :only => [:new]
+  resources :travellers, :only => [:show] do
+    resources :destinations, :only => [:new, :create]
+  end
+  resources :travellers, :only => [:show]
 end
