9eae32c90ef2accde2e44b6e425a2c596844590b
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-10 22:10:40 +0100

    set up defaults for devise routing for travellers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :travellers, :only => [:show] do
-    resources :destinations, :only => [:new, :create]
+    resources :destinations, :only => [:new, :create] do
+    end
   end
 end
