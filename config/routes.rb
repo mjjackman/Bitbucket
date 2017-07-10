6940e84fe36ff6106ec352f8fbe8bb48d4b91b18
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-10 19:39:54 +0100

    added devise, and added email and password to mocked traveller, as these are now required for devise to work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :travellers
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :travellers, :only => [:show] do
     resources :destinations, :only => [:new, :create]
   end
-  resources :travellers, :only => [:show]
 end
