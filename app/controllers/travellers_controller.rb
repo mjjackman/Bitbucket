b4fa463f6678d25e1199824c70cf1e24e281a8bb
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-11 12:53:38 +0100

    added edit_destination and update_destination to travellers_controller
    as the destinations are not being added by the traveller, but instead being added to their bucket list, I decided to move the methods for new and create to being member routes on the traveller controller. This removes the need for nested routes.

--- app/controllers/travellers_controller.rb
+++ app/controllers/travellers_controller.rb
@@ -2,7 +2,22 @@ class TravellersController < ApplicationController
 
   def show
     @traveller = Traveller.find(params[:id])
-    render 'show'
+    @traveller.destinations.new
   end
 
+  def edit_destination
+    @traveller = current_traveller
+    @destination = @traveller.destinations.new
+  end
+
+  def update_destination
+    @traveller = current_traveller
+    @traveller.destinations.create(traveller_destination_params)
+  end
+
+
+  private
+  def traveller_destination_params
+      params.require(:traveller).permit(destination_attributes: [:id])
+   end
 end
