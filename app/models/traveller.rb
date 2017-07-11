4300a5b5b81cde45650b00e4076cf786fb029f91
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-11 14:04:04 +0100

    changed nested attributes for travellers
    now accepting nested attributes for traveller_destinations, instead of destinations, as do't want to allow travellers to create destinations, only the relationship between them and the destinations.

--- app/models/traveller.rb
+++ app/models/traveller.rb
@@ -1,5 +1,5 @@
 class Traveller < ActiveRecord::Base
   has_many :traveller_destinations
   has_many :destinations, through: :traveller_destinations
-  accepts_nested_attributes_for :destinations
+  accepts_nested_attributes_for :traveller_destinations
 end
