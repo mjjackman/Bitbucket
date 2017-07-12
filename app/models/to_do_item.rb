b9dc9d780b8b75219afc24392fee2337c081147a
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-12 09:26:54 +0100

    changed todoitems model to work with geocode

--- app/models/to_do_item.rb
+++ app/models/to_do_item.rb
@@ -1,3 +1,11 @@
 class ToDoItem < ActiveRecord::Base
   belongs_to :destination
+  geocoded_by :geocode_input
+  validates :location, :presence => true
+  after_validation :geocode 
+
+  private
+  def geocode_input
+   "#{self.location}, #{self.destination.name}"
+  end
 end
