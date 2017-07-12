83779b63054e0496e6f436d4bf3c7f0385b009a1
Dan Garland <dan@wegotcoders.com>
2017-07-12 17:22:17 +0100

    Implemented todo list change on bounds change

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -4,4 +4,10 @@ class DestinationsController < ApplicationController
     @destination = Destination.find(params[:id])
   end
 
+  def index
+    @destinations = current_traveller.destinations.joins(:to_do_items).
+    where("latitude < ? AND latitude > ? AND longitude < ? AND longitude > ?", params[:north], params[:south], params[:east], params[:west])
+
+  end
+
 end
