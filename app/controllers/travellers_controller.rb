93ffad51135bfbce93bbdecaf4063bc8f56fced6
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-13 12:04:55 +0100

    cleaning up indentation

--- app/controllers/travellers_controller.rb
+++ app/controllers/travellers_controller.rb
@@ -3,7 +3,6 @@ class TravellersController < ApplicationController
   before_action :authenticate_traveller!
 
   def show
-    # binding.pry
     @traveller = Traveller.find(params[:id])
     @destinations = @traveller.destinations.all
     @traveller.destinations.new
