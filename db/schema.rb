ff34519ab91b19e57f26e8fc30e23569e9fa3134
Edd Williams <edd.williams@me.com>
2017-07-13 10:25:41 +0100

    Migration fix. Replaced create destinations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712095043) do
+ActiveRecord::Schema.define(version: 20170711151250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20170712095043) do
     t.string "location"
     t.float "latitude"
     t.float "longitude"
-    t.integer "priority"
   end
 
   create_table "traveller_destinations", force: :cascade do |t|
