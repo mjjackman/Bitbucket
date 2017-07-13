d92dcdc6e81b8824c0f127436d1c78e8ac7cfeb0
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-13 11:33:11 +0100

    added location stubs to to_do_items for geocoder. Tests now passing

--- features/bucketlist.feature
+++ features/bucketlist.feature
@@ -2,9 +2,10 @@ Feature: Bucket List
   @wip
   Scenario: A traveller sees all their bucketlist items ordered by priority
     Given they are registered
-      And they are signed in
-      And they have some destinations
-      And they have some to-dos
+      And they are on the sign in page
+      And they sign in
+      And they have multiple destinations
+      And they have multiple to-dos
       And the to-dos are given priorities
     When they visit their show page
     Then they see their destinations and to-dos
--- features/step_definitions/bucketlist_steps.rb
+++ features/step_definitions/bucketlist_steps.rb
@@ -1,8 +1,44 @@
+Given(/^they have multiple destinations$/) do
+  @destination = @traveller.destinations.create!(name: "China")
+  @destination2 = @traveller.destinations.create!(:name => "USA")
+
+end
+
+Given(/^they have multiple to\-dos$/) do
+  Geocoder::Lookup::Test.add_stub(
+    "Beijing, China", [
+      {
+        'latitude'     => 19.0759837,
+        'longitude'    => 72.8776559,
+        'address'      => 'Mumbai, Maharashtra, India',
+        'state'        => 'Maharashtra',
+        'state_code'   => '',
+        'country'      => 'India'
+      }
+    ]
+  )
+  @to_do_item = @destination.to_do_items.create!(:location => "Beijing", :activity => "Walk on top of the Great Wall")
+
+  Geocoder::Lookup::Test.add_stub(
+    "Arizona, USA", [
+      {
+        'latitude'     => 19.0759837,
+        'longitude'    => 72.8776559,
+        'address'      => 'Mumbai, Maharashtra, India',
+        'state'        => 'Maharashtra',
+        'state_code'   => '',
+        'country'      => 'USA'
+      }
+    ]
+  )
+  @to_do_item2 = @destination2.to_do_items.create!(:location => "Arizona", :activity => "Visit the Grand Canyon")
+  @to_do_item3 = @destination.to_do_items.create!(:location => "Beijing", :activity => "Visit the Forbidden City")
+end
+
 Given(/^the to\-dos are given priorities$/) do
   @to_do_item.update!(:priority => 2)
-  @to_do_item2 = ToDoItem.create!(:activity => "see the forbidden city", :priority => 1, :destination => @destination)
-  @destination2 = @traveller.destinations.create!(:name => "USA")
-  @to_do_item3 = ToDoItem.create!(:activity => "visit the Grand Canyon", :priority => 3, :destination => @destination2)
+  @to_do_item2.update!(:priority => 3)
+  @to_do_item3.update!(:priority => 1)
 end
 
 When(/^they visit their show page$/) do
